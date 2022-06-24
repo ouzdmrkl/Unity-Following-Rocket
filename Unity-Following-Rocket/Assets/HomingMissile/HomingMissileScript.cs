8b470edcb981697b83d33afc2a66f0bd78fe0b67
Oğuz Demirkol <ouzdmrkl@gmail.com>
2022-06-24 21:57:48 +0300

    Completed

---
+++ Unity-Following-Rocket/Assets/HomingMissile/HomingMissileScript.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class HomingMissileScript : MonoBehaviour
+{
+    private Transform target;
+    private new Rigidbody rigidbody;
+    
+    private void Awake()
+    {
+        target = GameObject.FindWithTag("Target").transform; // Get the target transform with tag
+        rigidbody = GetComponent<Rigidbody>(); // Get the rigidbody 
+    }
+
+    private void FixedUpdate()
+    {
+        FollowTarget();
+    }
+
+    private void FollowTarget()
+    {
+        var currentPosition = transform.position; // Current position of missile
+        var direction = (target.transform.position - currentPosition).normalized; // Get vector to rotate our missile
+        
+        transform.Rotate(-Vector3.Cross(direction, transform.up)); // Why I used cross?
+        // https://docs.unity3d.com/ScriptReference/Vector3.Cross.html
+
+        //transform.position += transform.up * 0.2f; // Missile moves with transform, not good enough
+        
+        rigidbody.AddForce(transform.up * rigidbody.mass * 17f); // If we move with rigidbody, missile goes better
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target")) // When rocket collides with target, explode
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
